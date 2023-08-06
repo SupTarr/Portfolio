812f451148b4e67deaee777e5123ec59fd36b7ad
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-08-06 16:26:42 +0700

    fix: remove log

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Link from "./Link";
 
 const Links = ({ destinations }) => {
-  console.log(destinations);
   return (
     <ul className="hidden lg:flex">
       {destinations.map((destination) => {
